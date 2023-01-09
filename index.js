1403131f8324aa86c9e6420ea31ab2f8ace51573
Minnatulislam <minnatulislam86@gmail.com>
2023-01-09 19:48:52 +0600

    jwt token is done

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000 ;
 
-// middele were 
+// middele were -------------
 
 app.use(cors());
 app.use(express.json())
